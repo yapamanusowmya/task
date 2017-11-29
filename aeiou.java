e32a6d205554c42e45b002fbd08e65ab71fd9815
yapamanusowmya <33864228+yapamanusowmya@users.noreply.github.com>
2017-11-29 11:05:16 +0530

    determining if it is a vowel or a consonant

---
+++ aeiou.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ciprus
+ */
+public class aeiou {
+public static void main(String[ ] arg)
+{
+Char c=new Char();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a character :");
+char in=sc.next( ).charAt(0);
+c.findVowelOrNot(in);
+}}
+
+
