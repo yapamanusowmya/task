e51fd441d4578736c45ab8cea06c25760d3c6b7a
yapamanusowmya <33864228+yapamanusowmya@users.noreply.github.com>
2017-11-29 16:21:41 +0530

    Determine if it is a prime number or not

---
+++ prime.java
@@ -0,0 +1,27 @@
+package task1;
+import java.util.Scanner;
+
+public class prime {
+public static void main(String args[]){    
+  int i,flag=0;      
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the number:");
+  int n=sc.nextInt();        
+  if(n==0||n==1)
+  {  
+   System.out.println(n+" is not prime number");      
+  }else
+  {  
+   for(i=2;i<=n/2;i++)
+   {      
+    if(n%i==0)
+    {      
+     System.out.println(n+" is not prime number");      
+     flag=1;      
+     break;      
+    }      
+   }      
+   if(flag==0)  { System.out.println(n+" is prime number"); }  
+  } 
+}    
+}
