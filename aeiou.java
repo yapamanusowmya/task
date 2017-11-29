61011ebe7ea8687f15f1426c41924ee2ec5e34c8
yapamanusowmya <33864228+yapamanusowmya@users.noreply.github.com>
2017-11-29 11:08:47 +0530

    Update aeiou.java

--- aeiou.java
+++ aeiou.java
@@ -1,16 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package Arrays;
 
 import java.util.Scanner;
-
-/**
- *
- * @author ciprus
- */
 public class aeiou {
 public static void main(String[ ] arg)
 {
@@ -22,3 +13,19 @@ c.findVowelOrNot(in);
 }}
 
 
+package Arrays;
+public class Char {
+    void findVowelOrNot(char ch)
+{
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+System.out.println("Entered character "+ch+" is  Vowel");
+}
+else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+System.out.println("Entered character "+ch+"is Consonent");
+      else
+System.out.println("Not an alphabet");
+
+}
+
+}
