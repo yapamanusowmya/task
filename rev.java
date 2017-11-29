c9f260f2158cbff1c419c7b6dc4ffd4cd531ac91
yapamanusowmya <33864228+yapamanusowmya@users.noreply.github.com>
2017-11-29 11:10:53 +0530

    Reverse of entered string

---
+++ rev.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ciprus
+ */
+public class rev {
+    public static void main(String args[])
+{
+String original, reverse = "" ;
+Scanner in = new Scanner(System.in);
+ 
+System.out.println("Enter a string to reverse");
+original = in.nextLine();
+ 
+int length = original.length();
+ 
+for ( int i = length - 1 ; i>= 0 ; i-- )
+reverse = reverse + original.charAt(i);
+ 
+System.out.println("Reverse of entered string is: "+reverse);
+    }
+
+}
